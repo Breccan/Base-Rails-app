7cff3f3b77747128a7b5559020740190a307e812
Breccan McLeod-Lundy <b.mcleodlundy@gmail.com>
2009-10-03 13:52:23 +1300

    Authlogic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  #Authlogic Routes
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new"
+  map.resource :account, :controller => "users"
+  map.resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
