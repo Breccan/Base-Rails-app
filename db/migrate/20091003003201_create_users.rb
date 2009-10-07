3d4342679614119e857d7d0a9c257fe51fe47c04
Breccan McLeod-Lundy <b.mcleodlundy@gmail.com>
2009-10-07 17:28:05 +1300

    migrations for postgres

--- db/migrate/20091003003201_create_users.rb
+++ db/migrate/20091003003201_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string    :login,               :null => false                # optional, you can use email instead, or both
-      t.string    :email,               :null => false                # optional, you can use login instead, or both
-      t.string    :crypted_password,    :null => false                # optional, see below
-      t.string    :password_salt,       :null => false                # optional, but highly recommended
-      t.string    :persistence_token,   :null => false                # required
-      t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
-      t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
+      t.string    :login,               :null => false, :default => ""                # optional, you can use email instead, or both
+      t.string    :email,               :null => false, :default => ""                # optional, you can use login instead, or both
+      t.string    :crypted_password,    :null => false, :default => ""                # optional, see below
+      t.string    :password_salt,       :null => false, :default => ""                # optional, but highly recommended
+      t.string    :persistence_token,   :null => false, :default => ""                # required
+      t.string    :single_access_token, :null => false, :default => ""                # optional, see Authlogic::Session::Params
+      t.string    :perishable_token, :default => "", :null => false  
 
       # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
       t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
--- db/migrate/20091003043757_add_users_password_reset_fields.rb
+++ db/migrate/20091003043757_add_users_password_reset_fields.rb
@@ -1,7 +1,5 @@
 class AddUsersPasswordResetFields < ActiveRecord::Migration
   def self.up
-    add_column :users, :perishable_token, :string, :default => "", :null => false  
-    add_column :users, :email, :string, :default => "", :null => false  
 
     add_index :users, :perishable_token  
     add_index :users, :email  
@@ -11,7 +9,5 @@ class AddUsersPasswordResetFields < ActiveRecord::Migration
     remove_index :users, :column => :email
     remove_index :users, :column => :perishable_token
 
-    remove_column :users, :email
-    remove_column :users, :perishable_token
   end
 end
